49d2c8d7d1a92c68e471f61d053971ee3d897566
CodeJxer <zhixinl@mails.ccnu.edu.cn>
2018-01-16 16:22:32 +0800

    complete _apis.py

--- app/spider/qqmusic.py
+++ app/spider/qqmusic.py
@@ -3,13 +3,24 @@
 import requests
 
 
-def search(page, word):
+def _search(page, word):
     url = 'https://c.y.qq.com/soso/fcgi-bin/client_search_cp?cr=1&p=' + page + '&n=20&w=' + word
     r = requests.get(url)
     text = r.content[9:-1]
     return text
 
 
+def _songdetail(songid):
+    url = 'https://c.y.qq.com/v8/fcg-bin/fcg_play_single_song.fcg?songmid=' + songid + '&tpl=' \
+          'yqq_song_detail&format=jsonp&callback=getOneSongInfoCallback'
+    r = requests.get(url)
+    text = r.content[23:-1]
+    return text
+
+def _albumdetail(albumid):
+    url = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_album_info_cp.fcg?albummid=' + albumid
+    r = requests.get(url)
+    return r.content
 
 
 # if __name__ == '__main__':
